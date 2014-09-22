47ce00511174af9ff4decf979f1bac546dcd4254
satojkovic <satojkovic@gmail.com>
2014-09-22 20:54:35 +0900

    Add pearson similarity

--- similarity_simple.go
+++ similarity_simple.go
@@ -43,12 +43,38 @@ func normalize(v Vector) (nv float64) {
 	return math.Sqrt(nv)
 }
 
+func checkVector(x Vector, y Vector) (err error) {
+	if len(x) == 0 || len(y) == 0 {
+		return errors.New("Length of vector is 0")
+	} else if isZeroVector(x) && isZeroVector(y) {
+		return errors.New("Both are zero vector")
+	} else {
+		return nil
+	}
+}
+
+func vSum(v Vector) float64 {
+	sum := 0.0
+	vsize := len(v)
+	for i := 0; i < vsize; i++ {
+		sum += v[i]
+	}
+	return sum
+}
+
+func vSumSq(v Vector) float64 {
+	sumSq := 0.0
+	vsize := len(v)
+	for i := 0; i < vsize; i++ {
+		sumSq += (v[i] * v[i])
+	}
+	return sumSq
+}
+
 func CosineSimilarity(v1 Vector, v2 Vector) (sim float64, err error) {
 	sim = 0.0
-	if len(v1) == 0 || len(v2) == 0 {
-		return sim, errors.New("Length of vector is 0")
-	} else if isZeroVector(v1) && isZeroVector(v2) {
-		return sim, errors.New("Both are zero vector")
+	if err := checkVector(v1, v2); err != nil {
+		return sim, err
 	}
 
 	dp, err := dotProduct(v1, v2)
@@ -57,6 +83,38 @@ func CosineSimilarity(v1 Vector, v2 Vector) (sim float64, err error) {
 	}
 
 	sim = dp / (normalize(v1) * normalize(v2))
+
+	return sim, nil
+}
+
+func PearsonSimilarity(x Vector, y Vector) (sim float64, err error) {
+	sim = 0.0
+	if err := checkVector(x, y); err != nil {
+		return sim, nil
+	}
+
+	dp, err := dotProduct(x, y)
+	if err != nil {
+		return sim, err
+	}
+
+	sumx := vSum(x)
+	sumy := vSum(y)
+
+	sumxSq := vSumSq(x)
+	sumySq := vSumSq(y)
+
+	n := float64(len(x))
+	num := dp - (sumx * sumy / n)
+	den := (sumxSq - math.Pow(sumx, 2)/n) *
+		(sumySq - math.Pow(sumy, 2)/n)
+	den = math.Sqrt(den)
+
+	if den == 0.0 {
+		return sim, errors.New("Pearson similarity could not calculated because of division by zero")
+	} else {
+		sim = num / den
+	}
 
 	return sim, nil
 }
