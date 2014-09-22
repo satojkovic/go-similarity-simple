3a0ece01189ec761f76889f20fd078a81fd5c7e8
satojkovic <satojkovic@gmail.com>
2014-09-22 21:00:54 +0900

    Add pearson similarity test

--- similarity_simple_test.go
+++ similarity_simple_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestSimilaritySimple(t *testing.T) {
-	v1 := Vector{0.789, 0.515, 0.335, 0.0}
-	v2 := Vector{0.832, 0.555, 0.0, 0.0}
+	x := Vector{0.789, 0.515, 0.335, 0.0}
+	y := Vector{0.832, 0.555, 0.0, 0.0}
 
-	sim, _ := CosineSimilarity(v1, v2)
+	sim, _ := CosineSimilarity(x, y)
 	expected_sim := 0.9421693704700748
 
 	if sim != expected_sim {
@@ -17,11 +17,23 @@ func TestSimilaritySimple(t *testing.T) {
 	}
 }
 
-func ExampleEmptySimilaritySimple() {
-	v1 := Vector{}
-	v2 := Vector{}
+func TestPersonSimilaritySimple(t *testing.T) {
+	x := Vector{1.0, 2.0, 3.0}
+	y := Vector{1.0, 3.0, 2.0}
 
-	_, err := CosineSimilarity(v1, v2)
+	sim, _ := PearsonSimilarity(x, y)
+	expected_sim := 0.5
+
+	if sim != expected_sim {
+		t.Errorf("got %v\nwant %v", sim, expected_sim)
+	}
+}
+
+func ExampleEmptySimilaritySimple() {
+	x := Vector{}
+	y := Vector{}
+
+	_, err := CosineSimilarity(x, y)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -29,10 +41,10 @@ func ExampleEmptySimilaritySimple() {
 }
 
 func ExampleZeroSimilaritySimple() {
-	v1 := Vector{0.0, 0.0, 0.0}
-	v2 := Vector{0.0, 0.0, 0.0}
+	x := Vector{0.0, 0.0, 0.0}
+	y := Vector{0.0, 0.0, 0.0}
 
-	_, err := CosineSimilarity(v1, v2)
+	_, err := CosineSimilarity(x, y)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -40,10 +52,10 @@ func ExampleZeroSimilaritySimple() {
 }
 
 func ExampleNotMatchSimilaritySimple() {
-	v1 := Vector{1.0, 2.0, 3.0}
-	v2 := Vector{1.0, 2.0, 3.0, 4.0}
+	x := Vector{1.0, 2.0, 3.0}
+	y := Vector{1.0, 2.0, 3.0, 4.0}
 
-	_, err := CosineSimilarity(v1, v2)
+	_, err := CosineSimilarity(x, y)
 	if err != nil {
 		fmt.Println(err)
 	}
